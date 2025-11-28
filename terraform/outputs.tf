76cb20e3c669a9a4c6eb2ca32c5ed544236f1551
rksurya-ai <seelamrakeshsurya3@gmail.com>
2025-11-27 22:30:12 -0800

    Add secure RDS PostgreSQL resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,13 @@ output "app_url" {
   description = "URL to access the application"
   value       = "http://${aws_instance.app_server.public_ip}:8501"
 }
+
+output "db_endpoint" {
+  description = "RDS PostgreSQL endpoint (not publicly accessible)"
+  value       = aws_db_instance.finance.endpoint
+}
+
+output "db_master_username" {
+  description = "Master username for the RDS instance"
+  value       = aws_db_instance.finance.username
+}
